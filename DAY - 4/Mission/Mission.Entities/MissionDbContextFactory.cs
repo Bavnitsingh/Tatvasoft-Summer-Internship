70168253d64b578e726a766eba6ffea37171d819
Bavnit Singh <chhabdabavnitsingh@gmail.com>
2025-07-07 16:55:08 +0530

    Authentication and Authorization using JWT

---
+++ DAY - 4/Mission/Mission.Entities/MissionDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Mission.Entities.Context;
+
+namespace Mission.Entities
+{
+    public class MissionDbContextFactory : IDesignTimeDbContextFactory<MissionDbContext>
+    {
+        public MissionDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MissionDbContext>();
+            optionsBuilder.UseNpgsql("host=localhost;port=5432;Database=Mission;Username=postgres;Password=A1B2");
+
+            return new MissionDbContext(optionsBuilder.Options);
+        }
+    }
+}
